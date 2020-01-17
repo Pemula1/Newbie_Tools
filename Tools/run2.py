d5bb70d0ec963bedce1e5012d83225805c825469
Pemula1 <59829400+Pemula1@users.noreply.github.com>
2020-01-17 16:41:10 +0700

    Update run2.py

--- Tools/run2.py
+++ Tools/run2.py
@@ -8,14 +8,7 @@ def slowprint(s):
 slowprint ('ASSALAMUALAIKUM WR WB')
 print
 slowprint('Selamat Datang User Di Newbie Tools')
-slowprint(' Gunakan Tools Dengan Bijak, Dan Mohon Untuk Kalian')
-slowprint(' Yang Tahu Username & Password Tools Ini, Mohon')
-slowprint(' Untuk Tidak Menyebarluaskan, Biarkan Mereka Yang')
-slowprint(' Usaha Mencari Username & Passwordnya Sendiri')
-slowprint(' Biar Mereka Jangan Jadi Orang Yang Pemalas')
-slowprint(' Gampang Njir!! Tinggal ketik 2 Kalian Tunggu DONE')
 print
-slowprint(' Hargai Author')
 slowprint(' Dan selamat bersenang-senang')
 print
 slowprint(' TERIMA KASIH YANG UDAH SUPPRORT GW')
